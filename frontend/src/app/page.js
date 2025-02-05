bfef25d92a700f3f6b20e10a34dd58a6f3236b43
alvvos <alvaro.salis@gmail.com>
2025-02-05 17:56:10 +0100

    Página de bienvenida

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,6 @@
+"use client";
+
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,9 +10,25 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <title>Mi Aplicación Next.js</title>
       </Head>
-      <div>Bienvenido al asistente de carrera de U-tad</div>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-800 transition-opacity duration-1000 opacity-0 animate-fadeIn">
+          Bienvenido al asistente de carrera de U-tad
+        </h1>
+      </div>
+      <style jsx>{`
+        @keyframes fadeIn {
+          0% {
+            opacity: 0;
+          }
+          100% {
+            opacity: 1;
+          }
+        }
+        .animate-fadeIn {
+          animation: fadeIn 1s forwards;
+        }
+      `}</style>
     </>
   );
 }
