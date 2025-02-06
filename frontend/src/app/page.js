4ca143235737a80c2c58d8302185aaea2de21354
AJG239 <alexjimgar2003@gmail.com>
2025-02-06 11:57:57 +0100

    Navbar
    Se crea el componente navbar y permite el acceso al apartado de login

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Bienvenido al asistente de carrera de U-tad</div>;
+  return (
+    <div>Bienvenido al asistente de carrera de U-tad</div>
+  )
 }
