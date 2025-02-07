c70344137ecf214920a45da13b27f6143f9d51c1
AJG239 <alexjimgar2003@gmail.com>
2025-02-07 12:49:49 +0100

    Fixed Navbar(Not at all)
    Se han añadido funcionalidades. Botón que devuelve al home, cuando se reduce la pantalla a la mitad se habré un menú hamburguesa y falta arreglar el desplegable

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Bienvenido al asistente de carrera de U-tad</div>;
+  return <div className="text-black">Bienvenido al asistente de carrera de U-tad</div>;
 }
